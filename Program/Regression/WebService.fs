1735f0b35f4d69fc6b4c33555dbfcfad0117eb55
Jesús Irais González Romero <laygr@outlook.com>
2017-07-04 20:00:33 -0500

    Update WebService.fs
    Fixed configuration. Added a check service

--- Program/Regression/WebService.fs
+++ Program/Regression/WebService.fs
@@ -5,22 +5,31 @@ open Suave.Filters
 open Suave.Operators
 open Suave.Http
 open Suave.Writers
+open Suave.Web
 
 let processRequest host port database username =
     choose [    
-      Suave.Filters.GET
-        >=> request (fun r ->
-              printf "Iniciando"
-              let results = Regression.regression host port database username
-              match r.queryParam "format" with
-              | Choice1Of2 "csv" -> Successful.OK (Regression.resultsToCSV results)
-                                    >=> setMimeType "application/csv; charset-utf-8"
-              | _                -> Successful.OK (Regression.resultsToJSON results)
-                                    >=> setMimeType "application/json; charset-utf-8"
-            )
+      Suave.Filters.GET >=>
+        choose [
+            path "/" >=>
+                request (fun r ->
+                      printf "Iniciando"
+                      let results = Regression.regression host port database username
+                      match r.queryParam "format" with
+                      | Choice1Of2 "csv" -> Successful.OK (Regression.resultsToCSV results)
+                                            >=> setMimeType "application/csv; charset-utf-8"
+                      | _                -> Successful.OK (Regression.resultsToJSON results)
+                                            >=> setMimeType "application/json; charset-utf-8"
+                    )
+            path "/check" >=> Successful.OK "It works!"
+          ]
     ]
 
+[<EntryPoint>]
 let main args =
-    let host, port, database, username = args.[0], args.[1], args.[2], args.[3]
-    startWebServer defaultConfig (processRequest host port database username)
+    let ip = args.[0]
+    let port =  System.Int32.Parse args.[1]
+    let serverConfig = Web.defaultConfig.withBindings [HttpBinding.createSimple Protocol.HTTP ip port]
+    let host, port, database, username = args.[2], args.[3], args.[4], args.[5]
+    startWebServer serverConfig (processRequest host port database username)
     0
